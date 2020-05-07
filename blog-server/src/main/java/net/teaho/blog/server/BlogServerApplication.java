302581a4e67ebd6f9be428b4129a20cd547505af
teaho2015 <teaho2015@gmail.com>
2020-05-07 16:47:33 +0800

    [upd]add ignore

--- blog-server/src/main/java/net/teaho/blog/server/BlogServerApplication.java
+++ blog-server/src/main/java/net/teaho/blog/server/BlogServerApplication.java
@@ -1,10 +1,12 @@
 package net.teaho.blog.server;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@MapperScan("net.teaho.blog.server")
 public class BlogServerApplication {
 
     public static void main(String[] args) {
