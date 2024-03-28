e683776f935f53a7707a50773888fc67668a8c41
teaho2015 <teaho2015@gmail.com>
2024-03-29 01:39:57 +0800

    remove component.

--- blog-front/src/pages/index.tsx
+++ blog-front/src/pages/index.tsx
@@ -3,7 +3,6 @@ import Layout from '@theme/Layout'
 import Hero from './_components/Hero'
 import BlogSection from './_components/BlogSection'
 import FeaturesSection from './_components/FeaturesSection'
-import HomepageProject from './_components/ProjectSection'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 
 export default function Home(): JSX.Element {
@@ -18,7 +17,6 @@ export default function Home(): JSX.Element {
         <Hero />
         <div className="container-wrapper">
           <BlogSection />
-          <HomepageProject />
           <FeaturesSection />
         </div>
       </main>
