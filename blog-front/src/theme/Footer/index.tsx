88fde96a5e28c83279f77dcf08bbb30ec27e22c0
teaho2015 <teaho2015@gmail.com>
2024-04-12 11:54:34 +0800

    performance.

--- blog-front/src/theme/Footer/index.tsx
+++ blog-front/src/theme/Footer/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Footer from '@theme-original/Footer'
-import { Analytics } from '@vercel/analytics/react'
 
 export default function FooterWrapper(props) {
   return (
     <>
       <Footer {...props} />
-      <Analytics />
+
     </>
   )
 }
